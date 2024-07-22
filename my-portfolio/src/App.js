0fb0c977bf8a19cdce48e1934c069ae250082b97
RayChik <vatoresp@gmail.com>
2024-07-22 12:51:06 +0300

    components-added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,69 +1,15 @@
 import "./styles/main.css";
 
-import sun from "./img/icons/sun.svg";
-import moon from "./img/icons/moon.svg";
-
-import vk from "./img/icons/vk.svg";
-import twitter from "./img/icons/twitter.svg";
-import instagram from "./img/icons/instagram.svg";
-import gitHub from "./img/icons/gitHub.svg";
-import linkedIn from "./img/icons/linkedIn.svg";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <div className="container">
-          <div className="nav-row">
-            <a href="./index.html" className="logo">
-              <strong>Freelancer</strong> portfolio
-            </a>
+      <Navbar />
 
-            <button className="dark-mode-btn">
-              <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-            </button>
-
-            <ul className="nav-list">
-              <li className="nav-list__item">
-                <a
-                  href="./index.html"
-                  className="nav-list__link nav-list__link--active"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./projects.html" className="nav-list__link">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./contacts.html" className="nav-list__link">
-                  Contacts
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <header className="header">
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            <strong>
-              Hi, my name is <em>Yuri</em>
-            </strong>
-            <br />a frontend developer
-          </h1>
-          <div className="header__text">
-            <p>with passion for learning and creating.</p>
-          </div>
-          <a href="#!" className="btn">
-            Download CV
-          </a>
-        </div>
-      </header>
+      <Header />
 
       <main className="section">
         <div className="container">
@@ -83,42 +29,7 @@ function App() {
         </div>
       </main>
 
-      <footer className="footer">
-        <div className="container">
-          <div className="footer__wrapper">
-            <ul className="social">
-              <li className="social__item">
-                <a href="#!">
-                  <img src={vk} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={instagram} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={twitter} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={gitHub} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={linkedIn} alt="Link" />
-                </a>
-              </li>
-            </ul>
-            <div className="copyright">
-              <p>© 2022 frontend-dev.com</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
 
       <script src="./js/main.js"></script>
     </div>
