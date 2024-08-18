102b6d0db8bfe95bed69bcdf18657b100ad0c167
RayChik <vatoresp@gmail.com>
2024-08-18 10:26:18 +0300

    dark-theme-added

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,10 +1,30 @@
+import { useEffect, useRef, useState } from "react";
 import "./BtnDarkMode.css";
 import moon from "./moon.svg";
 import sun from "./sun.svg";
 
 const BtnDarkMode = () => {
+  const [darkMode, setDarkMode] = useState("light");
+  const btnRef = useRef(null);
+
+  useEffect(() => {
+    if (darkMode === "dark") {
+      document.body.classList.add("dark");
+      btnRef.current.classList.add("dark-mode-btn--active");
+    } else {
+      document.body.classList.remove("dark");
+      btnRef.current.classList.remove("dark-mode-btn--active");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((current) => {
+      return current === "light" ? "dark" : "light";
+    });
+  };
+
   return (
-    <button className="dark-mode-btn">
+    <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
